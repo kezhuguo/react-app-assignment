5364ec162768c8bc3872377611d38b002d6b9075
Kezhu Guo <kezhuguo@gmail.com>
2021-09-08 01:16:45 -0500

    v1.1

--- interview_assign/src/backup/v1/components/Head.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Head = () => (
-  <h2 id="head" className="title">
-    Manage my items
-  </h2>
-);
-
-export default Head;
--- interview_assign/src/components/ItemsDiv.js
+++ interview_assign/src/components/ItemsDiv.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { CgClose } from "react-icons/cg";
 
 const ItemsDiv = (props) => {
   const items = props.items;
@@ -11,7 +10,20 @@ const ItemsDiv = (props) => {
         {items.map((item) => (
           <li key={item.id}>
             {item.name}
-            <CgClose id="close" onClick={() => props.passRemove(item.id)} />
+            <svg
+              id="close"
+              onClick={() => props.passRemove(item.id)}
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#000000"
+            >
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 
+                17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+              />
+            </svg>
           </li>
         ))}
       </ul>
